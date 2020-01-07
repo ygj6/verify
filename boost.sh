7365c4c499e39a6c620ff2fe82574b3afc3f37e7
yuangongji <yuangongji@foxmail.com>
2020-01-07 11:23:04 +0800

    Update main.yml

--- boost.sh
+++ boost.sh
@@ -2,38 +2,36 @@
 
 usage()
 {
-    cat <<EOL
+  cat <<EOL
   -b   - 32-bit or 64-bit library, maybe 32, 64 or both
-  -c   - the compiler, maybe gcc, clang or both
+  -t   - the toolset, maybe gcc, clang or both
 EOL
 }
 
 build_boost()
 {
-  BASE=`pwd`
-  ./b2 -j4 --prefix=${BASE}/usr --libdir="${BASE}/usr/lib$1" --with-chrono --with-context --with-filesystem --with-system --with-timer address-model=$1 install
+  BASE=`pwd`/..
+  ./b2 -j4 --toolset=$1 --prefix=${BASE}/usr --libdir="${BASE}/usr/$1/lib$2" --with-chrono --with-context --with-filesystem --with-system --with-timer address-model=$2 install
 }
 
 bit="64"
-cc="gcc"
+toolset="gcc"
 
-while getopts "b:c:" p; do
-  case "$p" in
+while getopts "b:t:" c; do
+  case "$c" in
     b)
       bit="$OPTARG"
       [ "$bit" != "32" ] && [ "$bit" != "64" ] && [ "$bit" != "both" ] && usage && exit 1
       ;;
-    c)
-      cc="$OPTARG"
-      [ "$cc" != "gcc" ] && [ "$cc" != "clang" ] && [ "$cc" != "both" ] && usage && exit 1
+    t)
+      toolset="$OPTARG"
+      [ "$toolset" != "gcc" ] && [ "$toolset" != "clang" ] && [ "$toolset" != "both" ] && usage && exit 1
       ;;
     ?*)
       echo "invalid arguments." && exit 1
       ;;
   esac
 done
-
-echo bit = $bit cc = $cc
 
 wget https://dl.bintray.com/boostorg/release/1.71.0/source/boost_1_71_0.tar.bz2
 tar xf boost_1_71_0.tar.bz2
@@ -42,19 +40,17 @@ cd boost_1_71_0
 
 build()
 {
-  if [[ "$bit" = "32" ]] || [[ "$bit" = "both" ]]; then
-    build_boost 32
-  fi
-  if [[ "$bit" = "64" ]] || [[ "$bit" = "both" ]]; then
-    build_boost 64
+  if [ "$bit" = "both" ]; then
+    build_boost $1 32
+    build_boost $1 64
+  else
+    build_boost $1 $bit
   fi
 }
 
-if [[ "$cc" = "gcc" ]] || [[ "$cc" = "both" ]]; then
-  export CC="gcc"
-  build
-fi
-if [[ "$cc" = "clang" ]] || [[ "$cc" = "both" ]]; then
-  export CC="clang"
-  build
+if [ "$toolset" = "both" ]; then
+  build gcc
+  build clang
+else
+  build $toolset
 fi
